2393019bec4954550600514f5450a80f9bf9f43f
KevinSuvagiya01 <kevin.s@mvclouds.com>
2024-03-14 00:22:50 +0530

    second

--- Filmic/src/utils/api.js
+++ Filmic/src/utils/api.js
@@ -1,21 +1,21 @@
-import axios from "axios";
+import axios from 'axios';
+const BASE_URL = 'https://api.themoviedb.org/3';
 
-const BASE_URL = "https://api.themoviedb.org/3";
 const TMDB_TOKEN = import.meta.env.VITE_APP_TMDB_TOKEN;
 
 const headers = {
-    Authorization: "bearer " + TMDB_TOKEN,
+    Authorization : "bearer" + TMDB_TOKEN,
 };
 
 export const fetchDataFromApi = async (url, params) => {
-    try {
-        const { data } = await axios.get(BASE_URL + url, {
+    try{
+        const{data} = await axios.get(BASE_URL + url + "?api_key=319d41685e62f52479dd1aeae493ded4", {
             headers,
-            params,
-        });
+            params
+        })
         return data;
-    } catch (err) {
+    }catch(err){
         console.log(err);
         return err;
     }
-};
+}
